eca0de25cd26171ff4a3cdf3fc371afb4fcb552f
yui-szk <yuisuzuki.017@gmail.com>
2023-12-20 01:45:41 +0900

    make home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Home</div>;
+  return <div>トーク一覧</div>;
 }
+
+// トーク一覧... /
+// トーク画面... 一対一＝ /相手のtalkID (or UUID)
+// タスク画面.../tasks
+
+// talk_all->/tasks
+// talk->tasks [talkID]/tasks
